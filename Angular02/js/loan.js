c4feacd772d23d8b5021ca740e56d57014984d8d
jaensoli <angel-andrey.jaen-solis@hp.com>
2015-06-15 23:02:53 -0600

    form validation in Loan Managemente

--- Angular02/js/loan.js
+++ Angular02/js/loan.js
@@ -3,37 +3,77 @@
  */
 var app= angular.module("loan",[]);
 
+
+//Directive copied from Angular JS We Page for validating number
+var INTEGER_REGEXP = /^\-?\d+$/;
+app.directive('integer', function() {
+    return {
+        require: 'ngModel',
+        link: function(scope, elm, attrs, ctrl) {
+            ctrl.$validators.integer = function(modelValue, viewValue) {
+                if (ctrl.$isEmpty(modelValue)) {
+                    // consider empty models to be valid
+                    return true;
+                }
+
+                if (INTEGER_REGEXP.test(viewValue)) {
+                    // it is valid
+                    return true;
+                }
+
+                // it is invalid
+                return false;
+            };
+        }
+    };
+});
+
+
+
 app.controller("ClientCtrl", function($scope){
 
    $scope.client=[
        {
            "name": "Andrey",
            "age":22,
-           "amount":1000
+           "amount":1000,
+           "email":"andrey@gmail.com"
 
        } ,
        {
            "name": "Melissa",
            "age":28,
-           "amount":2000
+           "amount":2000,
+           "email":"melissa@gmail.com"
        },
        {
            "name": "Deybi",
            "age":32,
-           "amount":500
+           "amount":500,
+           "email":"deybi@gmail.com"
 
        }
    ]
 
-    $scope.AddArray= function(){
-        $scope.client.push({"name":$scope.NewName,"age":$scope.NewAge,"amount":$scope.NewAmount});
-        $scope.NewName="";
-        $scope.NewAge="";
-        $scope.NewAmount="";
+    $scope.AddArray= function(isValid){
+
+        if(isValid){
+
+            $scope.client.push({"name":$scope.NewName,"age":$scope.NewAge,"amount":$scope.NewAmount,"email":$scope.NewEmail});
+
+        }
+
     }
 
     $scope.DeleteArray=function(index){
         $scope.client.splice(index,1);
+    }
+
+    $scope.Clear= function(){
+        $scope.NewName="";
+        $scope.NewAge="";
+        $scope.NewAmount="";
+        $scope.NewEmail="";
     }
 
 });
